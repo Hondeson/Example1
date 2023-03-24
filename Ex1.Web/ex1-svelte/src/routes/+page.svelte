2fcd962a3cc96e9cd59861d24c79e1e861232703
Jan Hrabec <hrabec7@gmail.com>
2023-03-24 19:02:35 +0100

    web na dockeru

---
+++ Ex1.Web/ex1-svelte/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { PUBLIC_API_PATH } from "$env/static/public";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<h2>{PUBLIC_API_PATH}</h2>
